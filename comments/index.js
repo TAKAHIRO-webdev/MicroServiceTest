426b61be59bc60129e84c8479ffd747daef1204b
TAKAHIRO ITO <takahiro.ito@vacancorp.com>
2022-03-20 06:13:28 +0900

    feat: create front-side of app

--- comments/index.js
+++ comments/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
 app.get("/posts/:id/comments", (req, res) => {
-  res.send(commentsByPostId[req.param.id] || []);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post("/posts/:id/comments", (req, res) => {
@@ -19,7 +21,7 @@ app.post("/posts/:id/comments", (req, res) => {
 
   comments.push({ id: commentId, content });
 
-  commentsByPostId[req.param.id] = comments;
+  commentsByPostId[req.params.id] = comments;
 
   res.status(201).send(comments);
 });
